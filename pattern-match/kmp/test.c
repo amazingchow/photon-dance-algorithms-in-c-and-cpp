7ad00d1a2f117bab8dbc44e63bae0ce7bff186e8
zhoujian2 <zhoujian2@sensetime.com>
2020-08-27 14:30:00 +0800

    experience gperftools with kmp

--- pattern-match/kmp/test.c
+++ pattern-match/kmp/test.c
@@ -1,12 +1,17 @@
+#include <gperftools/profiler.h>
 #include <stdio.h>
 #include "kmp.h"
 
 int main(int argc, char const *argv[])
 {
+    ProfilerStart("CPUPROFILE");
+
     const char* s = "oopabcabcabcabcefgmn";
     const char* p = "abcabcabcefg";
 
     printf("matched index i = %d\n", kmp(s, p));
 
     return 0;
+
+    ProfilerStop();
 }
